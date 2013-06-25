c0d676e72f110ef2449bc7800096d910449b410a
Michael Swartz <mswartz@gmail.com>
2013-06-24 23:53:19 -0400

    making some fun in CSS and JS.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,24 @@
 jQuery(document).ready(function($) {
 
-	// Your JavaScript goes here
+	//thought of this while playing with bgsize in inspector
+	function kenBurns(w, zoom){
+		var offset;
+		var z;
+
+		$(w).scroll(function(){
+			offset = $(window).scrollTop();
+			z = offset/zoom;
+
+			$('.page-intro').css(
+				'background-size',
+				120 - z + '%'
+			);
+		});
+	}
+
+	$(window).scroll(function(){
+		kenBurns(this, 40);
+	});
+	
 });
 
