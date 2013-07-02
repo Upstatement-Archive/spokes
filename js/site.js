f70acc3827d0c56211304ae41853395ddcf0a1e6
Michael Swartz <mswartz@gmail.com>
2013-07-02 12:10:11 -0400

    doing the nav animation

--- js/site.js
+++ js/site.js
@@ -1,30 +1,44 @@
-// jQuery(document).ready(function($) {
+jQuery(document).ready(function($) {
 
-// 	//thought of this while playing with bgsize in inspector
-// 	function kenBurns(w, zoom){
-// 		var offset;
-// 		var z;
+	//thought of this while playing with bgsize in inspector
+	function kenBurns(w, zoom){
+		var offset;
+		var z;
 
-// 		$(w).scroll(function(){
-// 			offset = $(window).scrollTop();
-// 			z = offset/zoom;
+		$(w).scroll(function(){
+			offset = $(window).scrollTop();
+			z = offset/zoom;
 
-// 			$('.page-intro').css(
-// 				'background-size',
-// 				120 - z + '%'
-// 			);
-// 		});
-// 	}
+			$('.page-intro').css(
+				'background-size',
+				120 - z + '%'
+			);
+		});
+	}
 
-// 	// $(window).scroll(function(){
-// 	// 	kenBurns(this, 40);
-// 	// });
+	function fadeNav(w){
+		var offset;
+
+		$(w).scroll(function(){
+			offset = $(window).scrollTop();
+
+			$('header').css(
+				'background-color',
+				'rgba(0,0,0,'+offset/500+')'
+			);
+		});
+	}
+
+	$(window).scroll(function(){
+		// kenBurns(this, 40);
+		fadeNav(this);
+	});
+
+
+	//Mobile nav toggling action-action-action
+	$(".m-nav-toggle").click(function(){
+	  	console.log('clicked');
+	    $("#access").toggleClass("visible");
+	});
 	
-// });
-
-jQuery(document).ready(function($){
-  $(".m-nav-toggle").click(function(){
-  	console.log('clicked');
-    $("#access").toggleClass("visible");
-  });
 });
